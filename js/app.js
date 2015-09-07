5400149f343e57b68dd8e86120f1c47e61a0fb4f
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-07 13:58:19 -0400

    changed links to pages and added footer

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
 $(document).ready(function() {
 
-  $('.port-img-1').mouseenter(function(){
-    $('.tags-all-1').slideDown('slow');
+  $('.port-img').mouseenter(function(){
+    $('.tags-all').slideDown('slow');
     })
   .mouseleave(function(){
-    $('.tags-all-1').slideUp('slow');
+    $('.tags-all').slideUp('slow');
   })
+
+  $('.port-img-2').mouseenter(function(){
+    $('.tags-all-2').slideDown('slow');
+    })
+  .mouseleave(function(){
+    $('.tags-all-2').slideUp('slow');
+  })
+
+  $('.port-img-3').mouseenter(function(){
+    $('.tags-all-3').slideDown('slow');
+    })
+  .mouseleave(function(){
+    $('.tags-all-3').slideUp('slow');
+  })
+
+  $('.port-img-4').mouseenter(function(){
+    $('.tags-all-4').slideDown('slow');
+    })
+  .mouseleave(function(){
+    $('.tags-all-4').slideUp('slow');
+  })
+
 
   function changeColor(element, curNumber){
     curNumber++;
